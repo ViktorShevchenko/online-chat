e16f01c3391f01152e7972f0b8665412f6eceadd
Viktor Shevchenko <viktor.shevchenko32@gmail.com>
2023-05-07 19:59:18 +0300

    feat-tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,18 @@
-import Demo from '@/components/views/Demo/Demo';
-import { Inter } from 'next/font/google';
-import React from 'react';
+import Icon from "@/components/shared/Icon/Icon";
+import Demo from "@/components/views/Demo/Demo";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-
-      <Demo>Hello Component | Our first component has been</Demo>
-
-      {/* <h1>Online chat</h1> */}
-
-      {/* <pre>
-        function Home() {
-          React.createElement('main', {},
-            React.createElement('h1', {}, 'Online chat')
-            )
-        }
-      </pre> */}
+      <Demo>
+        <Icon className="text-red-500" variant="attachment" /> Hello Component |
+        Our first component has been rendered
+      </Demo>
     </main>
   );
 }
