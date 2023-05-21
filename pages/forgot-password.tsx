5a97d8f611ea7294ff7f40602bda4286cf3d409f
Viktor Shevchenko <viktor.shevchenko32@gmail.com>
2023-05-21 21:20:56 +0300

    fix: correct tags <br

--- pages/forgot-password.tsx
+++ pages/forgot-password.tsx
@@ -9,12 +9,16 @@ const ForgotPassword = () => {
     <Layout className="flex flex-col">
       <form className="w-full px-14 pt-11 pb-8 rounded-3xl max-w-2xl m-auto mx-auto bg-slate-600 border border-gray-500">
         <Heading className="mb-5">
-          Забули <br></br>адресу електронної<br></br>пошти/пароль?
+          Забули <br />
+          адресу електронної
+          <br />
+          пошти/пароль?
         </Heading>
 
         <p className="text-lg text-center pb-8">
-          Ми надішлемо вам повідомлення з інструкціями <br></br>для скидання
-          пароля
+          Ми надішлемо вам повідомлення з інструкціями
+          <br />
+          для скидання пароля
         </p>
 
         <Input type="email" name="email" placeholder="name@example.com" />
@@ -22,10 +26,7 @@ const ForgotPassword = () => {
         <Button type="submit">Надішліть мені повідомлення</Button>
 
         <p className="text-lg text-center">
-          Згадали пароль?
-          <Link href="/sign-in">
-            <span> </span>Увійти
-          </Link>
+          Згадали пароль? <Link href="/sign-in">Увійти</Link>
         </p>
       </form>
     </Layout>
